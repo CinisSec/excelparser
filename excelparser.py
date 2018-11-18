f3faa2cb9a44234ff2982971c50adfd3adf56c72
CinisSec <gregorypro@protonmail.com>
2018-11-18 04:20:23 +0100

    small comment change

--- excelparser.py
+++ excelparser.py
@@ -14,7 +14,7 @@ sheet = wb.active
 exlist = []
 column = 'a'
 
-#Reading 7 headers, and asks for input
+#Reading headers, and asks for input
 for i in sheet['1']:
     print('Fill in the %s ' % (sheet[column+'1'].value))
     column = chr(ord(column.lower()) + 1).upper()
